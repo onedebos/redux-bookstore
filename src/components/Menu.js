f49ecde873be1dba9e6993ab3bf406200cee7ce9
Adebola <onedebos@gmail.com>
2020-01-28 18:23:42 +0100

    style menu

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Menu.css";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h1 className="menu-title">Bookstore CMS</h1>
+      <p className="books">BOOKS</p>
+      <p className="categories">CATEGORIES</p>
+    </div>
+  );
+};
+
+export default Menu;
