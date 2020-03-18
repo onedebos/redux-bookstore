212108421b5a28ef2f680b974770f35d08d79ca2
Adebola <onedebos@gmail.com>
2020-03-19 00:58:36 +0100

    fix redux issues

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,6 +1,5 @@
-const CREATE_BOOK = "CREATE_BOOK";
-const REMOVE_BOOK = "REMOVE_BOOK";
-const ADD_BOOK = "ADD_BOOK";
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
 
 const bookReducer = (state = null, action) => {
   switch (action.type) {
@@ -8,8 +7,6 @@ const bookReducer = (state = null, action) => {
       return [...state, action.book];
     case REMOVE_BOOK:
       return state.filter(storedBook => storedBook.id !== action.book.id);
-    case ADD_BOOK:
-      return state.addedBooks.concat(action.book);
     default:
       return state;
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
-import { combineReducers } from "redux";
-import booksReducer from "./books";
-import filterReducer from "./filter";
+import { combineReducers } from 'redux';
+import booksReducer from './books';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
   books: booksReducer,
-  filter: filterReducer,
-  addedBooks: booksReducer,
-  total: booksReducer
+  filter: filterReducer
 });
 
 export default rootReducer;
